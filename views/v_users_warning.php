64643a423d5ca8c469294e5f99bbfef40f2211ab
rfboyce <rfboyce@gmail.com>
2014-05-09 16:12:46 -0400

    formatted warning

--- views/v_users_warning.php
+++ views/v_users_warning.php
@@ -1,32 +1,17 @@
 <h2>You've been Hacked!</h2>
 <br>
 <div class="edit_profile_box">
-In one of the biggest password leaks on record, <a href="http://motherboard.vice.com/en_uk/blog/hackers-leaked-42-million-plaintext-dating-site-passwords">hackers
-exposed 42 million user accounts from the online dating site
-network Cupid Media based in Australia</a>, including names, email
-addresses, birthdays, and unencrypted passwords. And you'll
-never guess what the most popular passwords were. Oh wait, you
-totally will guess, because they're the most obvious passwords
-ever.<br>
+<h3>
+In one of the biggest password leaks on record, <a href="http://motherboard.vice.com/en_uk/blog/hackers-leaked-42-million-plaintext-dating-site-passwords">hackers exposed 42 million user accounts from the online dating site network Cupid Media based in Australia</a>, including names, email addresses, birthdays, and unencrypted passwords. And you'll never guess what the most popular passwords were. Oh wait, you totally will guess, because they're the most obvious passwords ever.<br>
 <br><br>
 
-Nearly 2 million of the love­seekers had chosen "123456" for their
-password, while another 1.2 million went with "11111." Some
-37,000 creatively chose "password" for their secret code. And
-somewhat heartwarmingly, 90,000 of the dating site users opted for
-"iloveyou." .<br>
+Nearly 2 million of the love­seekers had chosen "123456" for their password, while another 1.2 million went with "11111." Some 37,000 creatively chose "password" for their secret code. And somewhat heartwarmingly, 90,000 of the dating site users opted for "iloveyou." .<br>
 <br><br>
 
-Beyond that, a breach of MacRumors forums exposed 860,000
-passwords just over a week ago, and Github reported about the
-same time that user accounts with weak passwords were recently
-compromised in a brute force password­guessing attack, and 150
-million Adobe users’ password­identifying information was
-compromised as well.<br>
+Beyond that, a breach of MacRumors forums exposed 860,000 passwords just over a week ago, and Github reported about the same time that user accounts with weak passwords were recently compromised in a brute force password­guessing attack, and 150 million Adobe users’ password­identifying information was compromised as well.<br>
 <br><br>
 
-If you use the same passcode for multiple accounts, hackers can
-easily access your bank, email, and so on, paving the way for
-identity theft.<br>
+If you use the same passcode for multiple accounts, hackers can easily access your bank, email, and so on, paving the way for identity theft.<br>
 <br><br>
+</h3>
 </div>
