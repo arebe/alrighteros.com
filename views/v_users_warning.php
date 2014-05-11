4dd51e211e74d2c2b81e932320da1f8d75bf3cd7
rfboyce <rfboyce@gmail.com>
2014-05-11 12:32:09 -0400

    new text for warning page

--- views/v_users_warning.php
+++ views/v_users_warning.php
@@ -1,17 +1,37 @@
 <h2>You've been Hacked!</h2>
 <br>
 <div class="edit_profile_box">
-<h3>
-In one of the biggest password leaks on record, <a href="http://motherboard.vice.com/en_uk/blog/hackers-leaked-42-million-plaintext-dating-site-passwords">hackers exposed 42 million user accounts from the online dating site network Cupid Media based in Australia</a>, including names, email addresses, birthdays, and unencrypted passwords. And you'll never guess what the most popular passwords were. Oh wait, you totally will guess, because they're the most obvious passwords ever.<br>
-<br><br>
+<h3>Did you really need to save your credit card information?</h3>
 
-Nearly 2 million of the love­seekers had chosen "123456" for their password, while another 1.2 million went with "11111." Some 37,000 creatively chose "password" for their secret code. And somewhat heartwarmingly, 90,000 of the dating site users opted for "iloveyou." .<br>
-<br><br>
 
-Beyond that, a breach of MacRumors forums exposed 860,000 passwords just over a week ago, and Github reported about the same time that user accounts with weak passwords were recently compromised in a brute force password­guessing attack, and 150 million Adobe users’ password­identifying information was compromised as well.<br>
-<br><br>
+You’re making a purchase at awesomecheapstuff.com and the website offers the ability to create an account and save your credit card information for faster check-out later. <br>
+<br>
 
-If you use the same passcode for multiple accounts, hackers can easily access your bank, email, and so on, paving the way for identity theft.<br>
-<br><br>
-</h3>
+This might seem like a great idea, but perhaps you should consider the trustworthiness of the website you’re entrusting with your data. Would you give your credit card to a store clerk to keep in case you returned to make another purchase?<br>
+<br>
+
+<h3>Have you traded the security of your personal data for the convenience of a single log-in?</h3>
+
+
+Increasingly, websites ask you to create a log in for even the most mundane services. Social log-in applications allow web surfers to log into multiple sites using the same credentials as a widely-used social media site, like Facebook.<br>
+<br>
+
+However, these same social medias sites have put restrictions on their user base to disallow anonymity.  <a href="http://www.idtheftcenter.org/Cybersecurity/how-do-facebook-hacks-lead-to-identity-theft.html">Identity theft can start with an email address and password to one website which can snowball into access to more critical services and financial accounts</a>.  Banks often require birthday and hometown as security questions - all of which are handily provided by a Facebook profile.<br>
+<br>
+
+<h3>Are you using the same password across multiple sites?</h3>
+
+The recent Heartbleed bug potentially enabled attackers to intercept a “grab bag” of web server data - the contents of which could have included user names and passwords. While the bug did not affect most financial institutions, <a href="http://mashable.com/2014/04/09/heartbleed-bug-websites-affected/">security professionals recommend changing your password</a> on any accounts that might have shared the same password with services that were affected.<br>
+<br>
+
+<h3>How do you remember all of your passwords?</h3>
+
+A recent password leak from Australia’s Cupid Media offered <a href="http://krebsonsecurity.com/2013/11/cupid-media-hack-exposed-42m-passwords/">an opportunity to take a look at some of the most common passwords used across the company’s dating services</a>. Over 10% of the site’s 42 million users used one of 10 common passwords; for example, 1.9 million people used the phrase “123456” as their password for this site.<br>
+<br>
+
+
+Strong passwords aren’t always easy to remember but you can protect yourself and your data by using password management software such as <a href="http://keepass.info/">KeePass</a> or <a href="https://agilebits.com/onepassword">1Password</a>.<br>
+
+
+
 </div>
